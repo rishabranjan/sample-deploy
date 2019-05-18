c80903dc0e685b030515f2c80cd41694ea7dcfe9
rishabbaba17@gmail.com <rishabbaba10@gmail.com>
2019-05-19 04:29:40 +0530

    Updates

---
+++ precache-manifest.873ebbdbcbe2fe3562d02c094f05419c.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ce01fc5aa7ef2a1a71ac01f06b4de65e",
+    "url": "/sample-deploy/index.html"
+  },
+  {
+    "revision": "1e547c44ebf53ce8e564",
+    "url": "/sample-deploy/static/css/main.2cce8147.chunk.css"
+  },
+  {
+    "revision": "2e0129063922e3bb197a",
+    "url": "/sample-deploy/static/js/2.b41502e9.chunk.js"
+  },
+  {
+    "revision": "1e547c44ebf53ce8e564",
+    "url": "/sample-deploy/static/js/main.f5987717.chunk.js"
+  },
+  {
+    "revision": "0aac0f56bf12dd53b746",
+    "url": "/sample-deploy/static/js/runtime~main.85089631.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/sample-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
